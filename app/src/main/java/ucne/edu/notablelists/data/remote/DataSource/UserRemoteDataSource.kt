06c2f53cb90f9345c0cb9068929c06ab0ce56db3
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-11-21 12:20:53 -0600

    Borrado getUsers y getById

--- app/src/main/java/ucne/edu/notablelists/data/remote/DataSource/UserRemoteDataSource.kt
+++ app/src/main/java/ucne/edu/notablelists/data/remote/DataSource/UserRemoteDataSource.kt
@@ -9,33 +9,6 @@ import javax.inject.Inject
 class UserRemoteDataSource @Inject constructor(
     private val api: UserApiService
 ) {
-    suspend fun getUsers(): Resource<List<UserResponseDto>> {
-        return try {
-            val response = api.getUsers()
-            if (response.isSuccessful) {
-                response.body()?.let { Resource.Success(it) }
-                    ?: Resource.Error("Respuesta vacía del servidor")
-            } else {
-                Resource.Error("HTTP ${response.code()} ${response.message()}")
-            }
-        } catch (e: Exception) {
-            Resource.Error(e.localizedMessage ?: "Error de red")
-        }
-    }
-
-    suspend fun getUserById(id: Int): Resource<UserResponseDto> {
-        return try {
-            val response = api.getUserById(id)
-            if (response.isSuccessful) {
-                response.body()?.let { Resource.Success(it) }
-                    ?: Resource.Error("Respuesta vacía del servidor")
-            } else {
-                Resource.Error("HTTP ${response.code()} ${response.message()}")
-            }
-        } catch (e: Exception) {
-            Resource.Error(e.localizedMessage ?: "Error de red")
-        }
-    }
 
     suspend fun createUser(request: UserRequestDto): Resource<UserResponseDto> {
         return try {
