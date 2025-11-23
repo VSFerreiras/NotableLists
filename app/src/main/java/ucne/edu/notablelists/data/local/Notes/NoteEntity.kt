06bfc9d8fc25e86b677ddce630decf100550024a
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-11-22 19:40:04 -0600

    Creado NoteEntity.kt

---
+++ app/src/main/java/ucne/edu/notablelists/data/local/Notes/NoteEntity.kt
@@ -0,0 +1,22 @@
+package ucne.edu.notablelists.data.local.Notes
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.UUID
+
+@Entity(tableName = "notes")
+data class NoteEntity (
+    @PrimaryKey val id: String = UUID.randomUUID().toString(),
+    val remoteId: Int? = null,
+    val title: String,
+    val description: String,
+    val tag: String,
+    val isFinished: Boolean = false,
+    val reminder: String?,
+    val checklist: String?,
+    val priority: Int,
+    val deleteAt: String?,
+    val autoDelete: Boolean = false,
+    val isPendingCreate: Boolean = false
+)
+
