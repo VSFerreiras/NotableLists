71effb760f19d155d24fd4a233c186a483c4caee
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-11-22 19:32:14 -0600

    Creado NoteRequestDto.kt

---
+++ app/src/main/java/ucne/edu/notablelists/data/remote/dto/NoteRequestDto.kt
@@ -0,0 +1,13 @@
+package ucne.edu.notablelists.data.remote.dto
+
+data class NoteRequestDto(
+    val title: String,
+    val description: String,
+    val tag: String,
+    val isFinished: Boolean = false,
+    val reminder: String?,
+    val checklist: String?,
+    val priority: Int,
+    val deleteAt: String?,
+    val autoDelete: Boolean = false
+)
