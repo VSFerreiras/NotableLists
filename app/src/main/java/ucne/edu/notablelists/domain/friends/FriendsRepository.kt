0da0f51767b4e430924bbb694409a776bd141273
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-11-28 16:33:12 -0600

    Creado FriendsRepository.kt

---
+++ app/src/main/java/ucne/edu/notablelists/domain/friends/FriendsRepository.kt
@@ -0,0 +1,15 @@
+package ucne.edu.notablelists.domain.friends
+
+import ucne.edu.notablelists.data.remote.Resource
+import ucne.edu.notablelists.data.remote.dto.FriendDto
+import ucne.edu.notablelists.data.remote.dto.PendingRequestDto
+import ucne.edu.notablelists.data.remote.dto.UserResponseDto
+
+interface FriendsRepository {
+    suspend fun sendFriendRequest(userId: Int, friendId: Int): Resource<Unit>
+    suspend fun getPendingRequests(userId: Int): Resource<List<PendingRequestDto>>
+    suspend fun acceptFriendRequest(userId: Int, friendshipId: Int): Resource<Unit>
+    suspend fun getFriends(userId: Int): Resource<List<FriendDto>>
+    suspend fun getAllUsers(): Resource<List<UserResponseDto>>
+    suspend fun searchUsers(username: String): Resource<List<UserResponseDto>>
+}
