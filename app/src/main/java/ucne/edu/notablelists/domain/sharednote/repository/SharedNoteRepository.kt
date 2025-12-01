04f5374b20330587437aede5eabb66c5d07123e7
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-12-01 13:52:55 -0600

    feat: adds SharedNoteRepository.kt

---
+++ app/src/main/java/ucne/edu/notablelists/domain/sharednote/repository/SharedNoteRepository.kt
@@ -0,0 +1,19 @@
+package ucne.edu.notablelists.domain.sharednote.repository
+
+import ucne.edu.notablelists.data.remote.Resource
+import ucne.edu.notablelists.data.remote.dto.ShareResponseDto
+import ucne.edu.notablelists.data.remote.dto.SharedNoteByMeDto
+import ucne.edu.notablelists.data.remote.dto.SharedNoteWithDetailsDto
+import ucne.edu.notablelists.data.remote.dto.UpdateSharedStatusResponseDto
+
+interface SharedNoteRepository {
+    suspend fun shareNote(userId: Int, noteId: Int, friendId: Int): Resource<ShareResponseDto>
+    suspend fun getNotesSharedWithMe(userId: Int): Resource<List<SharedNoteWithDetailsDto>>
+    suspend fun getNotesSharedByMe(userId: Int): Resource<List<SharedNoteByMeDto>>
+    suspend fun updateSharedNoteStatus(userId: Int, sharedNoteId: Int): Resource<UpdateSharedStatusResponseDto>
+    suspend fun getSharedNoteDetails(userId: Int, noteId: Int): Resource<SharedNoteWithDetailsDto?>
+    suspend fun canAccessNote(userId: Int, noteId: Int): Resource<Boolean>
+    suspend fun getAllSharedNotes(userId: Int): Resource<Pair<List<SharedNoteWithDetailsDto>, List<SharedNoteByMeDto>>>
+    suspend fun syncSharedNotes(userId: Int): Resource<Unit>
+    suspend fun canShareNote(noteId: Int?): Resource<Boolean>
+}
