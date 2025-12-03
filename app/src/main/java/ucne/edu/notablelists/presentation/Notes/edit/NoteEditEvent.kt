525b0555ee46361f399b3012ab42c23b5cc13e2d
victor <vsferreiras402@protonmail.com>
2025-12-03 14:45:41 -0400

    Fix: corregido regresion del refactor en el event

--- app/src/main/java/ucne/edu/notablelists/presentation/Notes/edit/NoteEditEvent.kt
+++ app/src/main/java/ucne/edu/notablelists/presentation/Notes/edit/NoteEditEvent.kt
@@ -1,33 +1,30 @@
 package ucne.edu.notablelists.presentation.Notes.edit
 
 sealed interface NoteEditEvent {
-    data class EnteredTitle(val value: String) : NoteEditEvent
-    data class EnteredDescription(val value: String) : NoteEditEvent
-    data class EnteredTag(val value: String) : NoteEditEvent
-    data class ChangePriority(val value: Int) : NoteEditEvent
-    data class SetReminder(val date: Long, val timeHour: Int, val timeMinute: Int) : NoteEditEvent
-    data object AddChecklistItem : NoteEditEvent
-    data class UpdateChecklistItem(val index: Int, val text: String) : NoteEditEvent
-    data class ToggleChecklistItem(val index: Int) : NoteEditEvent
-    data class RemoveChecklistItem(val index: Int) : NoteEditEvent
-    data class ToggleFinished(val isFinished: Boolean) : NoteEditEvent
-    data object DeleteNote : NoteEditEvent
-    data object ShowDeleteDialog : NoteEditEvent
-    data object DismissDeleteDialog : NoteEditEvent
-    data object OnBackClick : NoteEditEvent
-    data object ClearReminder : NoteEditEvent
-    data object ShowTagSheet : NoteEditEvent
-    data object HideTagSheet : NoteEditEvent
-    data class SelectTag(val tag: String) : NoteEditEvent
-    data class CreateNewTag(val tag: String) : NoteEditEvent
-    data class DeleteAvailableTag(val tag: String) : NoteEditEvent
-    data object OnShareClick : NoteEditEvent
+    data class TitleChanged(val title: String) : NoteEditEvent
+    data class DescriptionChanged(val description: String) : NoteEditEvent
+    data class TagChanged(val tag: String) : NoteEditEvent
+    data class PriorityChanged(val priority: Int) : NoteEditEvent
+    data class ReminderSet(val date: Long, val hour: Int, val minute: Int) : NoteEditEvent
+    data object ReminderCleared : NoteEditEvent
+    data object ChecklistItemAdded : NoteEditEvent
+    data class ChecklistItemUpdated(val index: Int, val text: String) : NoteEditEvent
+    data class ChecklistItemToggled(val index: Int) : NoteEditEvent
+    data class ChecklistItemRemoved(val index: Int) : NoteEditEvent
+    data object SaveClicked : NoteEditEvent
+    data object BackClicked : NoteEditEvent
+    data object DeleteClicked : NoteEditEvent
+    data object DeleteConfirmed : NoteEditEvent
+    data object ShareClicked : NoteEditEvent
     data class ShareWithFriend(val friendId: Int) : NoteEditEvent
-    data object DismissShareDialogs : NoteEditEvent
-    data object NavigateToLogin : NoteEditEvent
-    data object NavigateToFriends : NoteEditEvent
-    data object ToggleCollaboratorMenu : NoteEditEvent
-    data class RequestRemoveCollaborator(val collaborator: ucne.edu.notablelists.presentation.Notes.edit.Collaborator) : NoteEditEvent
-    data object ConfirmRemoveCollaborator : NoteEditEvent
-    data object DismissRemoveCollaboratorDialog : NoteEditEvent
+    data object TagMenuClicked : NoteEditEvent
+    data class TagSelected(val tag: String) : NoteEditEvent
+    data class TagCreated(val tag: String) : NoteEditEvent
+    data class TagDeleted(val tag: String) : NoteEditEvent
+    data object CollaboratorMenuClicked : NoteEditEvent
+    data class RemoveCollaboratorRequested(val collaborator: Collaborator) : NoteEditEvent
+    data object RemoveCollaboratorConfirmed : NoteEditEvent
+    data object DialogDismissed : NoteEditEvent
+    data object LoginClicked : NoteEditEvent
+    data object FriendsClicked : NoteEditEvent
 }
