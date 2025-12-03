636fe6f7ed015298cd06abdb29a0bb43cd87cefa
victor <vsferreiras402@protonmail.com>
2025-12-03 14:39:25 -0400

    Refactor: removido NoteEditUiEvent, su funcionalidad fue replicada en el event principal

--- app/src/main/java/ucne/edu/notablelists/presentation/Notes/edit/NoteEditUiEvent.kt
+++
@@ -1,7 +0,0 @@
-package ucne.edu.notablelists.presentation.Notes.edit
-
-sealed interface NoteEditUiEvent {
-    data object NavigateBack : NoteEditUiEvent
-    data object NavigateToLogin : NoteEditUiEvent
-    data object NavigateToFriendList : NoteEditUiEvent
-}
